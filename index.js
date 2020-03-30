e6a5011d1edf1c91e708df3d509bd8f53133c464
Victor <adevic4christ@gmail.com>
2020-03-30 21:01:31 +0100

    ready for submission

--- index.js
+++ index.js
@@ -1,54 +1,9 @@
 const http = require("http"); //Require http module
-const path = require("path");
-const fs = require("fs");
-
-const collectRequestData = require("./collectRequestData");
-
-
-//Get the Form Template
-const filePath = path.join(__dirname, "public", "index.html");
-const formTemp = fs.readFileSync(filePath, "utf-8");
-
-//Get Not found file
+const requestAndResponseCycleHandler = require('./requestAndResponseController')
 
 
 //Create a http server
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.write(formTemp);
-    res.end();
-  } else if (req.url === "/message" && req.method === "POST") {
-    collectRequestData(req, result => {
-      const {
-        message
-      } = result;
-      fs.access(path.join(__dirname, "output", "message.txt"), (err) => {
-        if (err) {
-          console.log("The file does not exits.");
-          //make the directory
-          fs.mkdir(path.join(__dirname, "output"), {}, err => {
-            if (err) throw err;
-            //write message to file
-            fs.writeFile(path.join(__dirname, "output", "message.txt"), message, "utf-8", err => {
-              if (err) throw err;
-            })
-          });
-        } else {
-          console.log("File exits");
-          fs.appendFile(path.join(__dirname, "output", "message.txt"), `\n${message}`, "utf-8", err => err && err);
-        }
-      })
-      res.end(`<h1>Your data is collected successfully!</h1>
-      <a href="/"><button>Back</button></a>`);
-    })
-  } else {
-    res.write("<h1>Page Not Found</h1>");
-    res.writeHead(404, {
-      "Content-type": "html/text"
-    });
-    res.end();
-  }
-});
+const server = http.createServer(requestAndResponseCycleHandler);
 
 //PORT
 const PORT = 8080;
---
+++ requestAndResponseController.js
@@ -0,0 +1,67 @@
+const fs = require("fs");
+const path = require("path");
+
+//Get Request Data from the form
+const collectRequestData = require("./collectRequestData");
+
+
+//Get the Form Template
+const formFilePath = path.join(__dirname, "public", "index.html");
+const formTemp = fs.readFileSync(formFilePath, "utf-8");
+
+
+module.exports = (req, res) => {
+  if (req.url === "/") {
+    //send the form to the page
+    res.write(formTemp);
+    res.end();
+  } else if (req.url === "/message" && req.method === "POST") {
+    //collect form request data
+    collectRequestData(req, result => {
+      const {
+        message
+      } = result;
+      //check if the output directory and message.txt exist
+      //fs.access is used so as to not block the code. fs.access is async
+      fs.access(path.join(__dirname, "output", "message.txt"), (err) => {
+        if (err) {
+          console.info("The file does not exits.");
+          //make the directory
+          fs.mkdir(path.join(__dirname, "output"), {}, err => {
+            if (err) throw err;
+            //write message to file
+            fs.writeFile(path.join(__dirname, "output", "message.txt"), message, "utf-8", err => {
+              if (err) throw err;
+              //send success page
+              fs.readFile(path.join(__dirname, "public", "success.html"), "utf-8", (err, data) => {
+                if (err) throw err;
+                res.end(data)
+              });
+            });
+          });
+        } else {
+          //If folder and file exit, append text 
+          console.info("File exits");
+          fs.appendFile(path.join(__dirname, "output", "message.txt"), `\n${message}`, "utf-8", err => {
+            err && err;
+            //send success page
+            fs.readFile(path.join(__dirname, "public", "success.html"), "utf-8", (err, data) => {
+              if (err) throw err;
+              res.end(data);
+            });
+          });
+        }
+      });
+    });
+  } else {
+    //display page error if page not found
+    res.writeHead(404, {
+      "Content-type": "text/html"
+    });
+
+    fs.readFile(
+      path.join(__dirname, "public", "error-page.html"),
+      "utf-8",
+      (err, data) => res.end(data));
+  }
+}
